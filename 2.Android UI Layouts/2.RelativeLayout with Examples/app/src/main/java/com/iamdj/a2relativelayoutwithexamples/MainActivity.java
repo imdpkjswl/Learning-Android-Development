4080005329cb7b13b63d98088e94621f26f4c1e5
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-08-10 03:42:31 +0530

    Relative layout with example

---
+++ 2.Android UI Layouts/2.RelativeLayout with Examples/app/src/main/java/com/iamdj/a2relativelayoutwithexamples/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iamdj.a2relativelayoutwithexamples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
