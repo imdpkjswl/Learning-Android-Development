152088023171d149ba6b53c536dc2cb044b099b3
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-08-10 03:18:21 +0530

    learn linear layout with example

---
+++ 2.Android UI Layouts/1.LinearLayout with Examples/app/src/main/java/com/iamdj/a1linearlayoutwithexamples/MainActivity.java
@@ -0,0 +1,22 @@
+package com.iamdj.a1linearlayoutwithexamples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Perform action on button
+    public void sendMessage(View view){
+        Toast.makeText(this,"Message sent",Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
