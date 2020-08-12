a356e8edfe8a4c2066d79219a3973a8e382bb326
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-08-12 06:14:37 +0530

    ScrollView_IMPORTANT

---
+++ 4.Android UI Widgets/19.ScrollView/app/src/main/java/com/iamdj/a19scrollview/MainActivity.java
@@ -0,0 +1,16 @@
+package com.iamdj.a19scrollview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
