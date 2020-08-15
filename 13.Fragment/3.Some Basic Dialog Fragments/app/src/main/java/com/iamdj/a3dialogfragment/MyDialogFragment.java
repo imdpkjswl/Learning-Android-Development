eff53491198c32359557f73415a4bfaca6376ffe
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-08-15 06:01:48 +0530

    All Basic Fragment - IMPORTANT

---
+++ 13.Fragment/3.Some Basic Dialog Fragments/app/src/main/java/com/iamdj/a3dialogfragment/MyDialogFragment.java
@@ -0,0 +1,113 @@
+package com.iamdj.a3dialogfragment;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class MyDialogFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+
+        if (getArguments() != null) {
+            if (getArguments().getBoolean("notAlertDialog")) {
+                return super.onCreateDialog(savedInstanceState);
+            }
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Alert Dialog");
+        builder.setMessage("Alert Dialog inside DialogFragment");
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+
+        return builder.create();
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sample_dialog, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        final EditText editText = view.findViewById(R.id.inEmail);
+
+        if (getArguments() != null && !TextUtils.isEmpty(getArguments().getString("email")))
+            editText.setText(getArguments().getString("email"));
+
+        Button btnDone = view.findViewById(R.id.btnDone);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DialogListener dialogListener = (DialogListener) getActivity();
+                dialogListener.onFinishEditDialog(editText.getText().toString());
+                dismiss();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.d("API123", "onCreate");
+
+        boolean setFullScreen = false;
+        if (getArguments() != null) {
+            setFullScreen = getArguments().getBoolean("fullScreen");
+        }
+
+        if (setFullScreen)
+            setStyle(DialogFragment.STYLE_NORMAL, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    public interface DialogListener {
+        void onFinishEditDialog(String inputText);
+    }
+
+
+}
